03252087637eaeebfc6d0123c4e35c27e262180f
Alexander Minyaev <webvevar@gmail.com>
2018-09-14 10:47:15 +0700

    Edit RepositoryService (added singleton)

--- labaODBC_filmoteca/RepositoryService.cpp
+++ labaODBC_filmoteca/RepositoryService.cpp
@@ -1,33 +1,53 @@
-#include "pch.h"
+﻿#include "pch.h"
 #include "RepositoryService.h"
 
+RepositoryService::DB::DB()
+{
+	retcode = SQLAllocHandle(SQL_HANDLE_ENV, SQL_NULL_HANDLE, &henv);	//Init discriptor of environment
+	if (retcode != SQL_SUCCESS) {
+		cout << "Error SQLAllocHandle(SQL_HANDLE_ENV)" << endl;
+	}
 
-RepositoryService::RepositoryService()
+	retcode = SQLSetEnvAttr(henv, SQL_ATTR_ODBC_VERSION,				//Define version of ODBC
+		(void*)SQL_OV_ODBC3, 0);
+	if (retcode != SQL_SUCCESS) {
+		cout << "Error SQLSetEnvAttr" << endl;
+	}
+
+	retcode = SQLAllocHandle(SQL_HANDLE_DBC, henv, &hdbc);				//Init discriptor of conection 
+	if (retcode != SQL_SUCCESS) {
+		cout << "Error SQLAllocHandle(Conection)" << endl;
+	}
+}
+
+RepositoryService::DB::~DB()
+{
+
+}
+
+SQLRETURN RepositoryService::DB::getCodeReturn()
+{
+	return retcode;
+}
+
+vector<Film*>* RepositoryService::DB::getAllFilm()
+{
+
+	return nullptr;
+}
+
+void RepositoryService::DB::addFilm(Film film)
 {
 }
 
-
-RepositoryService::~RepositoryService()
+void RepositoryService::DB::editFilmById(int id)
 {
 }
 
-vector<Film> RepositoryService::getAllFilm()
-{
-	return vector<Film>();
-}
-
-void RepositoryService::addFilm(Film film)
+void RepositoryService::DB::removeFilmById(int id)
 {
 }
 
-void RepositoryService::editFilmById(int id)
-{
-}
-
-void RepositoryService::removeFilmById(int id)
-{
-}
-
-void RepositoryService::removeFilmByTitle(string title)
+void RepositoryService::DB::removeFilmByTitle(string title)
 {
 }
--- labaODBC_filmoteca/RepositoryService.h
+++ labaODBC_filmoteca/RepositoryService.h
@@ -7,25 +7,44 @@ using namespace std;
 #define BUFFER_SIZE 255
 
 class RepositoryService
-{
-	SQLHENV     henv;		// Descriptor of environment
-	SQLHDBC     hdbc; 		// Descriptor of conection
-	SQLHSTMT    hstmt; 		// Descriptor of operator
-	SQLRETURN   retcode; 	// Code of return
-	
+{	
+	class DB {
+
+		SQLHENV     henv;		// Descriptor of environment
+		SQLHDBC     hdbc; 		// Descriptor of conection
+		SQLRETURN   retcode; 	// Code of return
+
+	public:
+		
+		DB();
+		~DB();
+
+		SQLRETURN getCodeReturn();
+
+		vector<Film*>* getAllFilm();
+
+		void addFilm(Film film);
+
+		void editFilmById(int id);
+
+		void removeFilmById(int id);
+		void removeFilmByTitle(string title);
+
+
+
+	};
+
+	static DB* db;
 
 public:
-	RepositoryService();
-	~RepositoryService();
 
-	vector<Film> getAllFilm();
+	static DB* getDB() {
+		if (db == nullptr)
+		{
+			db = new DB();
+		}
+		return db;
+	}
 	
-	void addFilm(Film film);
-
-	void editFilmById(int id);
-
-	void removeFilmById(int id);
-	void removeFilmByTitle(string title);
-
 };
 
